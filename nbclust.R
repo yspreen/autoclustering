4ee27368dfbae56ac8b7ae11d4afcffc1137fa73
Yannick Spreen <yannick.spreen@gmail.com>
2019-12-17 17:50:27 -0500

    Make m, d, i args.

--- nbclust.R
+++ nbclust.R
@@ -2,22 +2,20 @@ install.packages("NbClust", repos='http://cran.r-project.org')
 
 cat("---- started ----\n") # all cat gets printed to the web ui
 library(NbClust)
-#library(parallel)
 warnings()
-cores <- 1
-cat("Nodes available: ", cores,"\n")
-cat("spawning cluster...\n")
-#cl4_1 <- makeCluster(cores, outfile="") # number of nodes to use in pool
 args = commandArgs(trailingOnly=TRUE)
-if(length(args)==0) {
-  stop("Input file must be supplied")
+if(length(args) < 4) {
+  stop("Usage: <method> <distance> <index> <filename>")
 }
 
 path = './'
 #path = 'C:\\Users\\Nicholas\\Downloads\\' # <-- this is only for testing purposes
 # ****** FILE INPUT ******
 cat("reading file...\n")
-file = args[1]
+file = args[4]
+m = args[1]
+d = args[2]
+i = args[3]
 
 # ************************
 #clear anything that's already there
@@ -27,20 +25,11 @@ system(paste("mkdir -p ", path, "partitions/", file, sep=""))
 mtb <- t(read.table(paste(path, file, '', sep=""), header=TRUE, row.names=1, sep="\t"))       # this reads the input file referenced above
 #print(mtb)
 
-methods <- c("kmeans","ward.D")
-distances <- c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")
-indexes <- c("kl", "ch", "hartigan", "ccc", "scott", "marriot", "trcovw", "tracew", "friedman", "rubin", "cindex", "db", "silhouette", "duda", "pseudot2", "beale", "ratkowsky", "ball", "ptbiserial", "gap", "frey", "mcclain", "gamma", "gplus", "tau", "dunn", "sdindex", "sdbw")
-#distances <- c("euclidean","manhattan") # used only when testing
-#indexes <- c("kl","cindex")             # ^
-
 cat("analyzing...\n")
 
-for (m in methods) {
   cat(paste(m,"\n"))
-  for (d in distances) {
     cat(">",d,"\n")
 
-    for (i in indexes) {
       cat('>>', i, '\n')
       warnings()
       tryCatch({
@@ -53,7 +42,6 @@ for (m in methods) {
       },
       warning = function(e) { cat(toString(paste("WARN: ",m,d,i,format(e),'', sep="\n"))); },
       error = function(e) { cat(toString(paste("ERR: ",m,d,i,format(e),'', sep="\n"))); })
-    }
+    
     cat("*>\n")
-  }
-}
+  
